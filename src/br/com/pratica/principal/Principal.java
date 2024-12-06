665df4a4222108ce0b3ae21ec34e724a267b422d
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-12-05 22:05:02 -0300

    Alterar iniciação do GSON, organizar e refatorar o código, adicionar conversão de livro para JSON e vice-versa

--- src/br/com/pratica/principal/Principal.java
+++ src/br/com/pratica/principal/Principal.java
@@ -3,42 +3,45 @@ package br.com.pratica.principal;
 import br.com.pratica.modelos.Editora;
 import br.com.pratica.modelos.Livro;
 import br.com.pratica.modelos.Pessoa;
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
+import java.sql.SQLOutput;
 
 public class Principal {
     public static void main(String[] args) {
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        Gson gson = new Gson();
 
-        String json = "{ \"Nome\": \"João\", \"Cidade\": \"Apucarana\", \"Cdade\": 30}";
+        System.out.println("DE JSON PARA PESSOA");
+        String jsonPessoa1 = "{ \"nome\": \"João\", " +
+                            "\"cidade\": \"Apucarana\", " +
+                            "\"idade\": 30}";
 
-        Pessoa pessoa1 = gson.fromJson(json, Pessoa.class);
-
+        Pessoa pessoa1 = gson.fromJson(jsonPessoa1, Pessoa.class);
         System.out.println(pessoa1);
-        String nome = pessoa1.nome();
-        String cidade = pessoa1.cidade();
-        int idade = pessoa1.idade();
 
-        System.out.println(nome);
-        System.out.println(cidade);
-        System.out.println(idade);
-
+        System.out.println("\nDE PESSOA PARA JSON");
         Pessoa pessoa2 = new Pessoa("Luiz", "Apucarana", 22);
 
-        String json2 = gson.toJson(pessoa2);
-        System.out.println(json2);
+        String jsonPessoa2 = gson.toJson(pessoa2);
+        System.out.println(jsonPessoa2);
 
-        System.out.println("----------------------------------------------");
+        System.out.println("\n----------------------------------------------\n");
 
+        System.out.println("DE LIVRO PARA JSON");
         Editora editora = new Editora("Rocco");
         Livro livro1 = new Livro("Fogo e sangue", "GRR Martin", editora);
 
-        String livro1JSON = gson.toJson(livro1);
-        System.out.println(livro1JSON);
+        String jsonLivro1 = gson.toJson(livro1);
+        System.out.println(jsonLivro1);
 
 
+        System.out.println("\nDE JSON PARA LIVRO");
+        String jsonLivro2 = "{\"titulo\": \"Harry Potter\", " +
+                            "\"autor\": \"JK Rowling\", " +
+                            "\"editora\": {\"nome\": \"Rocco\"}}";
+
+        Livro livro2 = gson.fromJson(jsonLivro2, Livro.class);
+        System.out.println(livro2);
     }
 }
