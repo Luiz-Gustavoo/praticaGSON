568d7a9231ae772918fb7e8e2f3535db9a05f5ac
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-12-05 22:02:38 -0300

    Adicionar método toString()

--- src/br/com/pratica/modelos/Pessoa.java
+++ src/br/com/pratica/modelos/Pessoa.java
@@ -1,4 +1,13 @@
 package br.com.pratica.modelos;
 
 public record Pessoa(String nome, String cidade, int idade) {
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome +
+                "\n" +
+                "Cidade: " + cidade +
+                "\n" +
+                "Idade: " + idade;
+    }
 }
