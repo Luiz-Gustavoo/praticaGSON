b7806d4a3c06936fb3c99d2fda945254d3c111c5
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-12-05 21:08:30 -0300

    Alterar a iniciação do GSON, adicionar objetos da classe Editora e Livro

--- src/br/com/pratica/principal/Principal.java
+++ src/br/com/pratica/principal/Principal.java
@@ -1,17 +1,23 @@
 package br.com.pratica.principal;
 
+import br.com.pratica.modelos.Editora;
+import br.com.pratica.modelos.Livro;
 import br.com.pratica.modelos.Pessoa;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Principal {
     public static void main(String[] args) {
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
 
-        String json = "{ \"Nome\": \"João\", \"cidade\": \"Apucarana\", \"idade\": 30}";
+        String json = "{ \"Nome\": \"João\", \"Cidade\": \"Apucarana\", \"Cdade\": 30}";
 
         Pessoa pessoa1 = gson.fromJson(json, Pessoa.class);
 
+        System.out.println(pessoa1);
         String nome = pessoa1.nome();
         String cidade = pessoa1.cidade();
         int idade = pessoa1.idade();
@@ -24,6 +30,14 @@ public class Principal {
 
         String json2 = gson.toJson(pessoa2);
         System.out.println(json2);
+
+        System.out.println("----------------------------------------------");
+
+        Editora editora = new Editora("Rocco");
+        Livro livro1 = new Livro("Fogo e sangue", "GRR Martin", editora);
+
+        String livro1JSON = gson.toJson(livro1);
+        System.out.println(livro1JSON);
 
 
     }
