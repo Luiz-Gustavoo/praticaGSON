f780cb322ee1f61d198d3569d47a0b05e254eaa7
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-06 20:54:59 -0300

    Adicionar classe Titulo

---
+++ src/br/com/pratica/modelos/Titulo.java
@@ -0,0 +1,13 @@
+package br.com.pratica.modelos;
+
+public class Titulo {
+    private String nome;
+    private String diretor;
+    private int duracaoMinutos;
+
+    public Titulo(String nome, String diretor, int duracaoMinutos) {
+        this.nome = nome;
+        this.diretor = diretor;
+        this.duracaoMinutos = duracaoMinutos;
+    }
+}
