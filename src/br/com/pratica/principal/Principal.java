f67ff2b08cfa44d5ac11606299ad037c7b80a3bf
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-06 22:00:33 -0300

    Adicionar objeto da classe ConverteJson, converter objeto Titulo para Json com o método paraJson

--- src/br/com/pratica/principal/Principal.java
+++ src/br/com/pratica/principal/Principal.java
@@ -1,18 +1,15 @@
 package br.com.pratica.principal;
 
+import br.com.pratica.modelos.ConverteJSon;
 import br.com.pratica.modelos.Titulo;
-import com.google.gson.Gson;
 
 public class Principal {
     public static void main(String[] args) {
 
-        Gson gson = new Gson();
-
         Titulo titulo1 = new Titulo("Django", "Scorcese", 60);
 
-        String tituloJSON = gson.toJson(titulo1);
-        System.out.println(tituloJSON);
-
+        ConverteJSon gson =  new ConverteJSon();
+        System.out.println(gson.paraJson(titulo1));
 
     }
 }
