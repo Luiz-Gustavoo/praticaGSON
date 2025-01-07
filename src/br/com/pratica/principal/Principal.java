f2b6439858cdb968e484d298da2c993d9a507567
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-06 21:05:49 -0300

    Instanciar objeto gson, instanciar objeto da classe Titulo, converter objeto Titulo para JSON

--- src/br/com/pratica/principal/Principal.java
+++ src/br/com/pratica/principal/Principal.java
@@ -1,7 +1,18 @@
 package br.com.pratica.principal;
 
+import br.com.pratica.modelos.Titulo;
+import com.google.gson.Gson;
+
 public class Principal {
     public static void main(String[] args) {
+
+        Gson gson = new Gson();
+
+        Titulo titulo1 = new Titulo("Django", "Scorcese", 60);
+
+        String tituloJSON = gson.toJson(titulo1);
+        System.out.println(tituloJSON);
+
 
     }
 }
